fcd5c7b34bb4cd8bd14114fe8aee51d91997b7ac
akaribrahim <bilalcanakar@gmail.com>
2020-09-10 15:03:32 +0300

    usage of react-router-dom

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent'
 import {DISHES} from '../shared/dishes';
-import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component{
     constructor(props) {
@@ -11,26 +12,27 @@ class Main extends Component{
 
         this.state = {
             dishes: DISHES,
-            selectedDish: null
         };
     }
 
     render(){
+        const HomePage = () => {
+            return(
+                <Home/>
+            );
+        }
+
         return (
             <div className="App">
                 <Header/>
-                <Menu 
-                    dishes={this.state.dishes}  
-                    onClick={(dishId) => this.onDishSelect(dishId)}
-                />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer/>
             </div>
         );
-    }
-
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
     }
 }
 
